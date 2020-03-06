fe963da811e61ff1c4432a5435c7a928edf5ed7b
lefcott19 <leandro.cotti@widergy.com>
2020-03-06 18:01:21 -0300

    wip

--- js/home/home.js
+++ js/home/home.js
@@ -2,10 +2,16 @@ const home = {};
 
 home.screen = "screen2";
 
-home.elements = {};
+home.elements = {
+  generalContainer: element.id("generalContainer"),
+  bigImage: element.class("bigImage")
+};
 
 home.resize = () => {
   const tabletRelation = [0.6188, 1];
+  // Get the min height of the 2 images in principal page
+  const containerTop = Math.min(...[...home.elements.bigImage].map(bigImage => bigImage.offsetHeight));
+  home.elements.generalContainer.style.top = `${containerTop}px`;
   if (innerWidth < innerHeight) {
     mobile.resize();
   } else {
@@ -18,7 +24,5 @@ home.resize = () => {
 };
 
 home.changeScreen = newScreen => {};
-
-// home.changeScreen(home.screen, home.screen);
 
 home.resize();
