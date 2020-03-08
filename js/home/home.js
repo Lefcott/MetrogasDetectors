0bbcaeaa70d4db7c1d3a3c4c6de835be2fe81098
Leandro Cotti <leandro.cotti@widergy.com>
2020-03-08 16:46:52 -0300

    Dynamic zoom. Icon and title

--- js/home/home.js
+++ js/home/home.js
@@ -1,19 +1,31 @@
 const home = {};
 
-home.screen = "screen2";
+home.screen = 'screen2';
 
 home.elements = {
-  generalContainer: element.id("generalContainer"),
-  bigImage: element.class("bigImage")
+  footer: element.id('footer'),
+  bigImage1: element.id('bigImage1'),
+  bigImage1Hidden: element.id('bigImage1Hidden')
+};
+
+home.constants = {
+  imgHeightCoef: 0.82,
+  bigImage1Relation: element.id('bigImage1Hidden').naturalHeight / element.id('bigImage1Hidden').naturalWidth,
+  bigImage2Relation: element.id('bigImage2Hidden').naturalHeight / element.id('bigImage2Hidden').naturalWidth
 };
 
 home.resize = () => {
   const tabletRelation = [0.6188, 1];
-  // Get the min height of the 2 images in principal page
-  const containerTop = Math.min(...[...home.elements.bigImage].map(bigImage => bigImage.offsetHeight));
-  
-  home.elements.generalContainer.style.top = `${0.82 * visualViewport.height}px`;
-  home.elements.generalContainer.style.height = `${0.18 * visualViewport.height}px`;
+  const { imgHeightCoef } = home.constants;
+  const imgWidth = innerWidth / 2;
+  const imgHeight = innerHeight * imgHeightCoef;
+  const maxSize = Math.max(imgWidth, imgHeight);
+
+  home.elements.footer.style.top = `${imgHeight}px`;
+  home.elements.footer.style.height = `${(1 - imgHeightCoef) * visualViewport.height}px`;
+
+  home.elements.bigImage1.setAttribute('height', imgHeight);
+  home.elements.bigImage1.setAttribute('viewBox', `0 0 ${imgWidth / maxSize * 100} ${imgHeight / maxSize * 100}`);
 
   if (innerWidth < innerHeight) {
     mobile.resize();
