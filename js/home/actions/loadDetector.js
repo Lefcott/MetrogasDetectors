972c4a21fef329cab282520e23b6ada378cbe062
lefcott19 <leandro.cotti@widergy.com>
2020-03-09 18:59:28 -0300

    wip

--- js/home/actions/loadDetector.js
+++ js/home/actions/loadDetector.js
@@ -5,14 +5,16 @@ actions.detector.buttonContainer = element.id('buttonDetectorContainer');
 
 actions.detector.load = () => {
   const { buttons, buttonContainer } = actions.detector;
-  const heightCoef = 0.14;
+  const heightCoef = 0.095;
 
-  buttonContainer.style.width = '50%';
+  buttonContainer.style.width = '48%';
   setTimeout(() => {
     const containerWidth = buttonContainer.offsetWidth;
     const buttonWidth = containerWidth / buttons.length;
 
     buttonContainer.style.height = `${containerWidth * heightCoef}px`;
+    console.log('buttonContainer.style.height', buttonContainer.style.height);
+    
     for (let k = 0; k < buttons.length; k += 1) {
       buttons[k].style.left = `${buttonWidth * k}px`;
       buttons[k].style.width = `${buttonWidth}px`;
