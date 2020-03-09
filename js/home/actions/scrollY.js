b178e3f5f9dac171b0afe142800fda237729fff6
Leandro Cotti <leandro.cotti@widergy.com>
2020-03-08 21:09:56 -0300

    Button scrolling Y

--- js/home/actions/scrollY.js
+++ js/home/actions/scrollY.js
@@ -1,10 +1,17 @@
+// -1 for up and 1 for down
 actions.scrollY = (direction, speed) => {
   document.body.scrollBy(0, direction * speed);
-  if (!(direction === 1 && document.body.scrollTop === document.body.scrollHeight - innerHeight)) {
-    requestAnimationFrame(() => {
-      actions.scrollY(direction, speed);
-    });
-  } else {
-    console.log('Finish scroll');
+  if (direction === 1) {
+    if (document.body.scrollTop !== document.body.scrollHeight - innerHeight) {
+      requestAnimationFrame(() => {
+        actions.scrollY(direction, speed);
+      });
+    }
+  } else if (direction === -1) {
+    if (document.body.scrollTop > 10) {
+      requestAnimationFrame(() => {
+        actions.scrollY(direction, speed);
+      });
+    }
   }
 };
