374f643d4ac64889327f7d59a6bce4fce68e3e23
lefcott19 <leandro.cotti@widergy.com>
2020-03-09 11:55:58 -0300

    Fixed buttons

--- js/home/actions/loadDetector.js
+++ js/home/actions/loadDetector.js
@@ -1,5 +1,20 @@
 actions.detector = {};
 
+actions.detector.buttons = element.class('detectorButton');
+actions.detector.buttonContainer = element.id('buttonDetectorContainer');
+
 actions.detector.load = () => {
-  console.log('Detector loaded');
+  const { buttons, buttonContainer } = actions.detector;
+  const heightCoef = 0.14;
+
+  buttonContainer.style.width = '50%';
+  setTimeout(() => {
+    const containerWidth = buttonContainer.offsetWidth;
+    const buttonWidth = containerWidth / buttons.length;
+
+    buttonContainer.style.height = `${containerWidth * heightCoef}px`;
+    for (let k = 0; k < buttons.length; k += 1) {
+      buttons[k].style.left = `${buttonWidth * k}px`;
+    }
+  }, 2);
 };
