f0f5946f3d2d8f6c413922fbe7adfe2d840825d4
lefcott19 <leandro.cotti@widergy.com>
2020-03-09 09:57:36 -0300

    Button animation

--- js/home/actions/playSound.js
+++ js/home/actions/playSound.js
@@ -1,13 +1,19 @@
 actions.sound = {};
-actions.sound.currButton;
+actions.sound.currButton = element.id('detectorButtonLeft');
+actions.sound.currButtonText = element.id('detectorButtonText1');
 
-actions.sound.play = {
-  play: (id, buttonId) => {
+actions.sound.play = (id, buttonId, buttonTextId) => {
+  if (id) {
     element.id(id).play();
-    if (actions.sound.currButton) {
-      actions.sound.currButton.style;
-    } else {
-      actions.sound.currButton = element.id(buttonId);
-    }
   }
+  if (actions.sound.currButton) {
+    actions.sound.currButton.removeClass('detectorButtonSelected');
+  }
+  if (actions.sound.currButtonText) {
+    actions.sound.currButtonText.removeClass('detectorButtonTextSelected');
+  }
+  actions.sound.currButton = element.id(buttonId);
+  actions.sound.currButton.addClass('detectorButtonSelected');
+  actions.sound.currButtonText = element.id(buttonTextId);
+  actions.sound.currButtonText.addClass('detectorButtonTextSelected');
 };
